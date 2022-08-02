5a6b849ed1bc51c348bc3a56ecdb7c3036f0485a
Sourcery AI <>
2022-08-02 19:04:53 +0000

    'Refactored by Sourcery'

--- custom-resources/attach_lambda_layer/lambda_handler.py
+++ custom-resources/attach_lambda_layer/lambda_handler.py
@@ -10,18 +10,18 @@ import functools
 
 def handler(event, context):
     responseData = {}
-    print('EVENT:[{}]'.format(event))
+    print(f'EVENT:[{event}]')
     try:
-        if event['RequestType'].upper() == 'UPDATE' or event['RequestType'].upper() == "CREATE":
+        if event['RequestType'].upper() in ['UPDATE', "CREATE"]:
             layer_name = event["ResourceProperties"]["LayerName"]
             lambda_name = event["ResourceProperties"]["LambdaName"]
 
             lambda_client = boto3.client('lambda')
-            
+
             # Find the latest version of the Lambda layer specified
             layer_versions = lambda_client.list_layer_versions(LayerName=layer_name)
             max_version = functools.reduce(lambda x, y: x if x["Version"] > y["Version"] else y, layer_versions["LayerVersions"])
-            
+
             # Retrieve all of the existing Lambda layers of the Lambda
             # and remove the one we want to update if it exists.
             lambda_configuration = lambda_client.get_function_configuration(FunctionName=lambda_name)
@@ -29,7 +29,12 @@ def handler(event, context):
 
             # Create a list that combines the existing layers without the one we want to update
             # and the newest version of the specified layer
-            lambda_layers = [] if len(existing_layers) == 0 else list(map(lambda x: x["Arn"], existing_layers))
+            lambda_layers = (
+                list(map(lambda x: x["Arn"], existing_layers))
+                if existing_layers
+                else []
+            )
+
             lambda_layers.append(max_version["LayerVersionArn"])
 
             # Update the Lambda layer
@@ -38,10 +43,7 @@ def handler(event, context):
 
             responseValue = 120
             responseData['Data'] = responseValue
-            cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
-
-        else:
-            cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
 
     except Exception as e:
         print("Lambda function: '{0}' has exception: {1}".format(lambda_name, str(e)))
